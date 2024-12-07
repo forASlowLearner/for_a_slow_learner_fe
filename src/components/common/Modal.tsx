901ca37c97480e52adf417d836276cdd35a58bd6
minibxx <alsdbqls1004@gmail.com>
2024-12-07 23:37:16 +0900

    feat: 모달 컴포넌트 children 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal({ children }: Props) {
             <div className='bg-[#363636e6] w-[100%] h-[100vh] relative'></div>
             <div className='absolute inset-0 flex items-center justify-center '>
                 <div className='bg-gray-200 p-4 rounded w-[310px]'>
-                {children}
+                    {children}  
                 </div>
             </div>
         </>
