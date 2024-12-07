b1cefe0360b5abc65acdec4ac6bc479f7b6580df
minibxx <alsdbqls1004@gmail.com>
2024-12-07 23:31:35 +0900

    feat: Modal component 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+    children: React.ReactNode;
+}
 
-export default function Modal({ }: Props) {
+export default function Modal({ children }: Props) {
     return (
         <>
             <div className='bg-[#363636e6] w-[100%] h-[100vh] relative'></div>
             <div className='absolute inset-0 flex items-center justify-center '>
                 <div className='bg-gray-200 p-4 rounded w-[310px]'>
+                {children}
                 </div>
             </div>
         </>
