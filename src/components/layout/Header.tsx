c5595f7423e12f01fb6ac733b101036791c6e745
hyerini <rinlee2001@naver.com>
2024-12-09 02:54:21 +0900

    feat(components): Header 컴포넌트 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,37 @@
-const Header = () => {
-  return <h1 className="py-8">Header</h1>;
+import { useNavigate } from "react-router-dom";
+import { UserRound, ChartColumn } from "lucide-react";
+import Logo from "../../assets/images/mainLogo.png";
+
+interface HeaderProps {
+    backgroundWidth?: string;
+}
+
+const Header = ({ backgroundWidth }: HeaderProps) => {
+    const navigate = useNavigate();
+
+    return (
+        <div
+            className={`flex flex-row justify-between items-center px-7 py-3`}
+            style={{
+                width: backgroundWidth || "100%",
+            }}
+        >
+            <img src={Logo} alt="Main Logo"
+                 className="h-10 cursor-pointer"
+                 onClick={() => navigate('/')}
+            />
+            <div className="flex flex-row gap-4">
+                <ChartColumn
+                    className="font-black cursor-pointer"
+                    onClick={() => navigate('/rank')}
+                />
+                <UserRound
+                    className="font-black cursor-pointer"
+                    onClick={() => navigate('/mypage')}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Header;
