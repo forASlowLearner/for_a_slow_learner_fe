2010f70954a991ff4412e821899435f4025376e1
hyerini <rinlee2001@naver.com>
2024-12-09 02:54:45 +0900

    feat(layout): Layout 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainLayout from './../components/layout/MainLayout';
+import MainLayout from './../components/layout/Layout';
 import BackGround from './../assets/images/backgroundImg.png';
 
 const Home = () => {
