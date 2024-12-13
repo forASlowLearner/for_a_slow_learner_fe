b3b564e8efbdace571a0dcd98dc5448aee3def96
minibxx <alsdbqls1004@gmail.com>
2024-12-13 14:34:40 +0900

    feat: 랭킹 페이지 화면 구현

--- src/components/rank/RankingID.tsx
+++ src/components/rank/RankingID.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import Typography from '../common/Typography';
-import RankProfile from '../assets/images/rankProfile.png';
+import RankProfile from '../../assets/images/rankProfile.png';
 type Props = {}
 
 function RankingID({ }: Props) {
     return (
         <>
-            <div className='px-[20px] py-[10px]'>
-            <img src={RankProfile}/>
-            <div>
-                <Typography title='닉네임' type='small1' />
+            <div className='px-[30px] py-[7px] flex justify-between'>
+                <div className='flex gap-[5%] items-center'>
+                    <Typography title='1' type='body4' />
+                    <img src={RankProfile} />
+                    <div>
+                        <Typography title='닉네임' type='small1' />
+                        <Typography title='(abcabc@kakao.com)' type='small3' />
+                    </div>
+                </div>
+                <div className='mt-[8px]'>
+                    <Typography title='150점' type='body4' />
+                </div>
             </div>
-            </div>
-
+            
         </>
     )
 }
