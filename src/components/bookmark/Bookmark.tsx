1ce01c7915e828219bdaf73c47e8d966c032550b
minibxx <alsdbqls1004@gmail.com>
2024-12-16 02:36:44 +0900

    feat: 카테고리 커버 이미지 추가

--- src/components/bookmark/Bookmark.tsx
+++ src/components/bookmark/Bookmark.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import cBlue from '../../assets/images/categoryBlue.png';
+import cYellow from '../../assets/images/categoryYellow.png';
 
 interface MainLayoutProps {
-    backgroundWidth?: string; // 배경 이미지 너비
+  backgroundWidth?: string; // 배경 이미지 너비
 }
 
 const Bookmark = ({ backgroundWidth }: MainLayoutProps) => {
-    return (
-       <div>hi</div>
-    );
+  return (
+    <>
+      <div className='grid grid-cols-2 gap-[5%] '>
+        <img className='w-[100%]' src={cBlue} />
+        <img className='w-[100%]' src={cYellow} />
+        <img className='w-[100%]' src={cYellow} />
+        <img className='w-[100%]' src={cBlue} />
+        <img className='w-[100%]' src={cBlue} />
+        <img className='w-[100%]' src={cYellow} />
+      </div>
+    </>
+  );
 };
 
 export default Bookmark;
