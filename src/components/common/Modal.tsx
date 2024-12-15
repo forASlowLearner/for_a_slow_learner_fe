74278a8c5797020be9ec7c4213463631761e99a8
hyerini <rinlee2001@naver.com>
2024-12-16 02:28:34 +0900

    feat(common): Modal 컴포넌트 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
+    isOpen: boolean;
+    onClose: () => void;
     children: React.ReactNode;
-}
+};
 
-export default function Modal({ children }: Props) {
+export default function Modal({ isOpen, onClose, children }: Props) {
+    if (!isOpen) return null;
+
     return (
         <>
-            <div className='bg-[#363636e6] w-[100%] h-[100vh] relative'></div>
-            <div className='absolute inset-0 flex items-center justify-center '>
-                <div className='bg-gray-200 p-4 rounded w-[310px]'>
-                    {children}  
+            <div
+                className="bg-[#363636e6] fixed inset-0 z-40"
+                onClick={onClose}
+            ></div>
+
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="bg-gray-200 p-4 rounded w-[310px] relative">
+                    {children}
+                    <button
+                        onClick={onClose}
+                        className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+                    >
+                        ✕
+                    </button>
                 </div>
             </div>
         </>
-    )
+    );
 }
