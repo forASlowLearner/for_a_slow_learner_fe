477c61dc2321fb1ae24e77d272864e26ea69ba2b
hyerini <rinlee2001@naver.com>
2025-01-15 00:41:58 +0900

    feat(pages): Quiz 페이지 구현

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,12 +1,33 @@
+// Quiz.tsx
 import React from "react";
-import MainLayout from './../components/layout/Layout';
-import TestStep from "./../components/question/TestStep";
-import BackGround from "./../assets/images/backgroundImg.png";
+import MainLayout from "../components/layout/Layout";
+import BackGround from "../assets/images/backgroundImg.png";
+import ProgressBar from "../components/question/ProgressBar";
+import TestStep from "../components/question/TestStep";
+import useTest from "../hooks/useTest";
 
 const Quiz = () => {
+    const {
+        currentQuestionIndex,
+        currentQuestion,
+        userAnswer,
+        isAnswered,
+        handleAnswerChange,
+        handleNextQuestion,
+        handleSubmitAnswer,
+    } = useTest();
+
     return (
         <MainLayout backgroundImage={BackGround}>
-            <TestStep />
+            <ProgressBar currentIndex={currentQuestionIndex} totalQuestions={9} />
+            <TestStep
+                currentQuestion={currentQuestion}
+                userAnswer={userAnswer}
+                isAnswered={isAnswered}
+                handleAnswerChange={handleAnswerChange}
+                handleNextQuestion={handleNextQuestion}
+                handleSubmitAnswer={handleSubmitAnswer}
+            />
         </MainLayout>
     );
 };
