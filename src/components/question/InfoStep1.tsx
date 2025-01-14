49a084c619fb15e4faa858b6859d677df0a4c00e
hyerini <rinlee2001@naver.com>
2025-01-15 00:40:21 +0900

    feat(question): quiz 관련 컴포넌트 구현

--- src/components/question/InfoStep1.tsx
+++ src/components/question/InfoStep1.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Balloon from "../question/Balloon";
 import Typography from "../common/Typography";
+import Input from "../common/Input";
+import Button from "../common/Button";
 
 interface Step1Props {
     name: string;
@@ -10,25 +12,26 @@ interface Step1Props {
 
 const Step1 = ({ name, setName, handleNameSubmit }: Step1Props) => {
     return (
-        <>
+        <div className="px-4">
             <Balloon arrowPosition="center">
                 <Typography
                     title='안녕하세요😳 저는 당신과 함께 걸어갈 표표입니다! 제가 당신을 뭐라고 부르면 될까요?' type='body5' />
             </Balloon>
-            <input
+            <Input
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 placeholder="이름을 입력하세요"
                 className="mt-4 p-2 border rounded"
             />
-            <button
+            <Button
+                color="green"
                 onClick={handleNameSubmit}
-                className="mt-4 p-2 bg-blue-500 text-white rounded"
+                className="mt-4 p-2 rounded"
             >
                 입력하기
-            </button>
-        </>
+            </Button>
+        </div>
     );
 };
 
